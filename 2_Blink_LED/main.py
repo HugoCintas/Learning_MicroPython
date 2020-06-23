2e9a6284c132705904fafe2371f422d85c90462c
Hugo <hugo.cintas@gmail.com>
2020-06-23 11:44:41 +0200

    Tutorial 2 finished
    Play a little bit with the function 
    Allow the blue and the green led to blink at differents speeds

--- 2_Blink_LED/main.py
+++ 2_Blink_LED/main.py
@@ -24,9 +24,39 @@ led_green(0) # turn the pin off
 led_green(1) # turn the pin on
 
 
+# to play
+led_blue = pyb.Pin('LED_BLUE', pyb.Pin.OUT)
+
+blue_counter = 0
+blue_period = 500
+green_counter = 0
+green_period = 1000
+counter = 0
+
 # programm
 while True:
-	led_green.on()
-	sleep(1)
-	led_green.off()
-	sleep(1)
+	# led_green.on()
+	# sleep(1)
+	# led_green.off()
+	# sleep(1)
+
+	# check the status of the blue led
+	if counter >= blue_counter:
+		# check the value of the led
+		if led_blue.value() == 0:
+			led_blue.on()
+		else:
+			led_blue.off()
+		blue_counter = counter + blue_period # fix new time to make change
+
+	# check the status of the green led
+	if counter >= green_counter:
+		# check the value of the led
+		if led_green.value() == 0:
+			led_green.on()
+		else:
+			led_green.off()
+		green_counter = counter + green_period # fix new time to make change
+
+	sleep(0.001)
+	counter += 1
